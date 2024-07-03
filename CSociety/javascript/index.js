f6da62875441db02cbed4f8cf508244e6dc1ebfc
joelmurray0 <144688121+joelmurray0@users.noreply.github.com>
2024-07-03 19:13:50 +0100

    rejigg

---
+++ CSociety/javascript/index.js
@@ -0,0 +1,9 @@
+console.log("Hello world") // print function
+
+let x = 5; // variable declaration
+const NAME = "Joel"; // constant declaration
+const num_array = [0,1,2,3,4,5,6,7,8,9] // array syntax
+
+function function_name(){
+     console.log("I'm in a function!")
+};
